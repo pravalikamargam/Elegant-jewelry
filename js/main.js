8c3c3ef74525c4c76b2bc0388524ce44bdebb7c4
pravalika <pravallikamargam39@gmail.com>
2026-01-29 00:08:46 +0530

    main

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let slides=document.querySelectorAll(".slide");
+let i=0;
+setInterval(()=>{
+slides[i].classList.remove("active");
+i=(i+1)%slides.length;
+slides[i].classList.add("active");
+},3000);
+
